a397e2ca3c7010003b9c1d60c91a47aa5a2f3812
MatinJiridity <104725671+MatinJiridity@users.noreply.github.com>
2023-04-18 13:47:35 +0300

    ToDo List

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# T0D0 List Project
+### react redux toolkit query
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,46 @@
+// intract with api
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const apiSlice = createApi({
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500'}),
+    tagTypes: ['Todos'],    
+    endpoints: (builder) => ({
+        getTodos : builder.query({
+            query: () => '/todos',
+            transformResponse: res => res.sort((a,b) => b.id - a.id),
+            providesTags: ['Todos']
+        }),
+        addTodos: builder.mutation({
+            query: (todo) => ({
+                url: '/todos',
+                method: 'POST',
+                body: todo
+            }),
+            invalidatesTags: ['Todos']
+        }),
+        updateTodos: builder.mutation({
+            query: (todo) => ({
+                url: `/todos/${todo.id}`,
+                method: 'PATCH',
+                body: todo
+            }),
+            invalidatesTags: ['Todos']
+        }),
+        deleteTodos: builder.mutation({
+            query: ({id}) => ({
+                url: `/todos/${id}`,
+                method: 'DELETE',
+                body: id
+            }),
+            invalidatesTags: ['Todos']
+        }),
+    })
+})
+
+export const {
+    useGetTodosQuery,
+    useAddTodosMutation,
+    useDeleteTodosMutation,
+    useUpdateTodosMutation
+} = apiSlice
